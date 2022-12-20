59c6e05442e538de052f568839745d2a68d71d73
maxim.stepanov <maxim.stepanov@4finance.com>
2022-12-20 07:49:57 +0200

    Add Dog class

---
+++ src/com/javastudycourse/Dog.java
@@ -0,0 +1,27 @@
+//package com.javastudycourse;
+//
+//public class Dog {
+//
+//    private final String s ;
+//    private boolean flag;
+//
+//    public Dog(String s) {
+//        this.s = s;
+//    }
+//
+//    public String getS() {
+//        return s;
+//    }
+//
+//    public Boolean getFlag() {
+//        return flag;
+//    }
+//
+//    public void setS(String s) {
+//        this.s = s;
+//    }
+//
+//    public void setFlag(Boolean flag) {
+//        this.flag = flag;
+//    }
+//}
